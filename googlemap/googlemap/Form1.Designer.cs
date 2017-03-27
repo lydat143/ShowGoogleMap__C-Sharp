67e25d2d7a95b067269303e9822b88cf041e1f72
Dat <lydat143@gmail.com>
2017-03-27 17:17:26 +0700

    commit 3
    hiển thị tọa độ theo địa chỉ
    hiển thị địa chỉ theo tọa độ
    hiển thị tọa độ theo địa chỉ IP

--- googlemap/googlemap/Form1.Designer.cs
+++ googlemap/googlemap/Form1.Designer.cs
@@ -29,24 +29,30 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bntGet = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbtnGetIp = new System.Windows.Forms.RadioButton();
+            this.rbtnGetAddress = new System.Windows.Forms.RadioButton();
+            this.rbtnGetCoordinate = new System.Windows.Forms.RadioButton();
+            this.grbCoordinate = new System.Windows.Forms.GroupBox();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnCoordinate = new System.Windows.Forms.RadioButton();
+            this.rbtnAddr = new System.Windows.Forms.RadioButton();
             this.btnOK = new System.Windows.Forms.Button();
             this.txtAddress = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.webBView = new System.Windows.Forms.WebBrowser();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.rbtnAdd = new System.Windows.Forms.RadioButton();
-            this.rbtnCoordinate = new System.Windows.Forms.RadioButton();
-            this.grbCoordinate = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtLat = new System.Windows.Forms.TextBox();
-            this.txtLong = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.grbCoordinate.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -57,6 +63,8 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.bntGet);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
             this.splitContainer1.Panel1.Controls.Add(this.grbCoordinate);
             this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
             this.splitContainer1.Panel1.Controls.Add(this.btnOK);
@@ -70,9 +78,147 @@
             this.splitContainer1.SplitterDistance = 341;
             this.splitContainer1.TabIndex = 0;
             // 
+            // bntGet
+            // 
+            this.bntGet.Location = new System.Drawing.Point(123, 350);
+            this.bntGet.Name = "bntGet";
+            this.bntGet.Size = new System.Drawing.Size(75, 23);
+            this.bntGet.TabIndex = 8;
+            this.bntGet.Text = "Get";
+            this.bntGet.UseVisualStyleBackColor = true;
+            this.bntGet.Click += new System.EventHandler(this.bntGet_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbtnGetIp);
+            this.groupBox2.Controls.Add(this.rbtnGetAddress);
+            this.groupBox2.Controls.Add(this.rbtnGetCoordinate);
+            this.groupBox2.Location = new System.Drawing.Point(12, 236);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(306, 108);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Get info:";
+            // 
+            // rbtnGetIp
+            // 
+            this.rbtnGetIp.AutoSize = true;
+            this.rbtnGetIp.Location = new System.Drawing.Point(16, 75);
+            this.rbtnGetIp.Name = "rbtnGetIp";
+            this.rbtnGetIp.Size = new System.Drawing.Size(143, 17);
+            this.rbtnGetIp.TabIndex = 8;
+            this.rbtnGetIp.TabStop = true;
+            this.rbtnGetIp.Text = "From My Ip to Coordinate";
+            this.rbtnGetIp.UseVisualStyleBackColor = true;
+            this.rbtnGetIp.CheckedChanged += new System.EventHandler(this.rbtnGetIp_CheckedChanged);
+            // 
+            // rbtnGetAddress
+            // 
+            this.rbtnGetAddress.AutoSize = true;
+            this.rbtnGetAddress.Location = new System.Drawing.Point(16, 52);
+            this.rbtnGetAddress.Name = "rbtnGetAddress";
+            this.rbtnGetAddress.Size = new System.Drawing.Size(155, 17);
+            this.rbtnGetAddress.TabIndex = 7;
+            this.rbtnGetAddress.TabStop = true;
+            this.rbtnGetAddress.Text = "From Coordinate to Address";
+            this.rbtnGetAddress.UseVisualStyleBackColor = true;
+            this.rbtnGetAddress.CheckedChanged += new System.EventHandler(this.rbtnGetAddress_CheckedChanged);
+            // 
+            // rbtnGetCoordinate
+            // 
+            this.rbtnGetCoordinate.AutoSize = true;
+            this.rbtnGetCoordinate.Location = new System.Drawing.Point(16, 29);
+            this.rbtnGetCoordinate.Name = "rbtnGetCoordinate";
+            this.rbtnGetCoordinate.Size = new System.Drawing.Size(149, 17);
+            this.rbtnGetCoordinate.TabIndex = 6;
+            this.rbtnGetCoordinate.TabStop = true;
+            this.rbtnGetCoordinate.Text = "From Address to Cordinate";
+            this.rbtnGetCoordinate.UseVisualStyleBackColor = true;
+            this.rbtnGetCoordinate.CheckedChanged += new System.EventHandler(this.rbtnGetCoordinate_CheckedChanged);
+            // 
+            // grbCoordinate
+            // 
+            this.grbCoordinate.Controls.Add(this.txtLong);
+            this.grbCoordinate.Controls.Add(this.txtLat);
+            this.grbCoordinate.Controls.Add(this.label3);
+            this.grbCoordinate.Controls.Add(this.label2);
+            this.grbCoordinate.Location = new System.Drawing.Point(12, 100);
+            this.grbCoordinate.Name = "grbCoordinate";
+            this.grbCoordinate.Size = new System.Drawing.Size(306, 97);
+            this.grbCoordinate.TabIndex = 6;
+            this.grbCoordinate.TabStop = false;
+            this.grbCoordinate.Text = "Coordinate:";
+            // 
+            // txtLong
+            // 
+            this.txtLong.Location = new System.Drawing.Point(51, 58);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(239, 20);
+            this.txtLong.TabIndex = 9;
+            // 
+            // txtLat
+            // 
+            this.txtLat.Location = new System.Drawing.Point(51, 28);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(239, 20);
+            this.txtLat.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Long:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Lat:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnCoordinate);
+            this.groupBox1.Controls.Add(this.rbtnAddr);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 45);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search by:";
+            // 
+            // rbtnCoordinate
+            // 
+            this.rbtnCoordinate.AutoSize = true;
+            this.rbtnCoordinate.Location = new System.Drawing.Point(186, 19);
+            this.rbtnCoordinate.Name = "rbtnCoordinate";
+            this.rbtnCoordinate.Size = new System.Drawing.Size(76, 17);
+            this.rbtnCoordinate.TabIndex = 5;
+            this.rbtnCoordinate.TabStop = true;
+            this.rbtnCoordinate.Text = "Coordinate";
+            this.rbtnCoordinate.UseVisualStyleBackColor = true;
+            this.rbtnCoordinate.CheckedChanged += new System.EventHandler(this.rbtnCoordinate_CheckedChanged);
+            // 
+            // rbtnAddr
+            // 
+            this.rbtnAddr.AutoSize = true;
+            this.rbtnAddr.Location = new System.Drawing.Point(16, 19);
+            this.rbtnAddr.Name = "rbtnAddr";
+            this.rbtnAddr.Size = new System.Drawing.Size(63, 17);
+            this.rbtnAddr.TabIndex = 4;
+            this.rbtnAddr.TabStop = true;
+            this.rbtnAddr.Text = "Address";
+            this.rbtnAddr.UseVisualStyleBackColor = true;
+            this.rbtnAddr.CheckedChanged += new System.EventHandler(this.rbtnAdd_CheckedChanged);
+            // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(115, 212);
+            this.btnOK.Location = new System.Drawing.Point(123, 203);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(75, 23);
             this.btnOK.TabIndex = 2;
@@ -105,86 +251,6 @@
             this.webBView.Size = new System.Drawing.Size(588, 415);
             this.webBView.TabIndex = 0;
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.rbtnCoordinate);
-            this.groupBox1.Controls.Add(this.rbtnAdd);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(306, 45);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Search by:";
-            // 
-            // rbtnAdd
-            // 
-            this.rbtnAdd.AutoSize = true;
-            this.rbtnAdd.Location = new System.Drawing.Point(16, 19);
-            this.rbtnAdd.Name = "rbtnAdd";
-            this.rbtnAdd.Size = new System.Drawing.Size(63, 17);
-            this.rbtnAdd.TabIndex = 4;
-            this.rbtnAdd.TabStop = true;
-            this.rbtnAdd.Text = "Address";
-            this.rbtnAdd.UseVisualStyleBackColor = true;
-            this.rbtnAdd.CheckedChanged += new System.EventHandler(this.rbtnAdd_CheckedChanged);
-            // 
-            // rbtnCoordinate
-            // 
-            this.rbtnCoordinate.AutoSize = true;
-            this.rbtnCoordinate.Location = new System.Drawing.Point(186, 19);
-            this.rbtnCoordinate.Name = "rbtnCoordinate";
-            this.rbtnCoordinate.Size = new System.Drawing.Size(76, 17);
-            this.rbtnCoordinate.TabIndex = 5;
-            this.rbtnCoordinate.TabStop = true;
-            this.rbtnCoordinate.Text = "Coordinate";
-            this.rbtnCoordinate.UseVisualStyleBackColor = true;
-            this.rbtnCoordinate.CheckedChanged += new System.EventHandler(this.rbtnCoordinate_CheckedChanged);
-            // 
-            // grbCoordinate
-            // 
-            this.grbCoordinate.Controls.Add(this.txtLong);
-            this.grbCoordinate.Controls.Add(this.txtLat);
-            this.grbCoordinate.Controls.Add(this.label3);
-            this.grbCoordinate.Controls.Add(this.label2);
-            this.grbCoordinate.Location = new System.Drawing.Point(12, 100);
-            this.grbCoordinate.Name = "grbCoordinate";
-            this.grbCoordinate.Size = new System.Drawing.Size(306, 97);
-            this.grbCoordinate.TabIndex = 6;
-            this.grbCoordinate.TabStop = false;
-            this.grbCoordinate.Text = "Coordinate:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Lat:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 61);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Long:";
-            // 
-            // txtLat
-            // 
-            this.txtLat.Location = new System.Drawing.Point(51, 28);
-            this.txtLat.Name = "txtLat";
-            this.txtLat.Size = new System.Drawing.Size(239, 20);
-            this.txtLat.TabIndex = 7;
-            // 
-            // txtLong
-            // 
-            this.txtLong.Location = new System.Drawing.Point(51, 58);
-            this.txtLong.Name = "txtLong";
-            this.txtLong.Size = new System.Drawing.Size(239, 20);
-            this.txtLong.TabIndex = 9;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -199,10 +265,12 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.grbCoordinate.ResumeLayout(false);
             this.grbCoordinate.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -217,11 +285,16 @@
         private System.Windows.Forms.GroupBox grbCoordinate;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton rbtnCoordinate;
-        private System.Windows.Forms.RadioButton rbtnAdd;
+        private System.Windows.Forms.RadioButton rbtnAddr;
         private System.Windows.Forms.TextBox txtLong;
         private System.Windows.Forms.TextBox txtLat;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbtnGetAddress;
+        private System.Windows.Forms.RadioButton rbtnGetCoordinate;
+        private System.Windows.Forms.Button bntGet;
+        private System.Windows.Forms.RadioButton rbtnGetIp;
     }
 }
 
--- googlemap/googlemap/Form1.cs
+++ googlemap/googlemap/Form1.cs
@@ -9,19 +9,96 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Win32;
 using System.Web;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
 
 namespace googlemap
 {
     public partial class Form1 : Form
     {
+        string IpAddress = "";
         public Form1()
         {
             InitializeComponent();
             SetWebBrowserVersion(11);
             this.webBView.Navigate("http://maps.google.com/maps");
-            rbtnAdd.Checked = true;
+            rbtnAddr.Checked = true;
+            getIpAddress(ref IpAddress);
         }
+        
+        #region class of Json
+                public class AddressComponent
+                {
+                    public string long_name { get; set; }
+                    public string short_name { get; set; }
+                    public List<string> types { get; set; }
+                }
 
+                public class Location
+                {
+                    public double lat { get; set; }
+                    public double lng { get; set; }
+                }
+
+                public class Northeast
+                {
+                    public double lat { get; set; }
+                    public double lng { get; set; }
+                }
+
+                public class Southwest
+                {
+                    public double lat { get; set; }
+                    public double lng { get; set; }
+                }
+
+                public class Viewport
+                {
+                    public Northeast northeast { get; set; }
+                    public Southwest southwest { get; set; }
+                }
+
+                public class Geometry
+                {
+                    public Location location { get; set; }
+                    public string location_type { get; set; }
+                    public Viewport viewport { get; set; }
+                }
+
+                public class Result
+                {
+                    public List<AddressComponent> address_components { get; set; }
+                    public string formatted_address { get; set; }
+                    public Geometry geometry { get; set; }
+                    public string place_id { get; set; }
+                    public List<string> types { get; set; }
+                }
+
+                public class RootObject
+                {
+                    //jSon của https://maps.googleapis.com/maps/api/geocode/json?address=
+                    public List<Result> results { get; set; }
+                    public string status { get; set; }
+
+                    //Json của https://api.ipify.org/?format=json
+                    public string ip { get; set; }
+
+                    //Json của http://freegeoip.net/json
+                    //public string ip { get; set; }
+                    public string country_code { get; set; }
+                    public string country_name { get; set; }
+                    public string region_code { get; set; }
+                    public string region_name { get; set; }
+                    public string city { get; set; }
+                    public string zip_code { get; set; }
+                    public string time_zone { get; set; }
+                    public double latitude { get; set; }
+                    public double longitude { get; set; }
+                    public int metro_code { get; set; }
+                }
+
+        #endregion
 
         #region set_lastest_version_IE_on_webBrowser 
         // Set a registry DWORD value.
@@ -51,39 +128,144 @@ namespace googlemap
             string app_name = System.AppDomain.CurrentDomain.FriendlyName;
 
             // You can do both if you like.
-            //SetRegistryDword(key64bit, app_name, ie_version);
-            SetRegistryDword(key32bit, app_name, ie_version);
+            SetRegistryDword(key64bit, app_name, ie_version);
+            //SetRegistryDword(key32bit, app_name, ie_version);
         }
         #endregion
-        
-        private string GetAddress()
-        {
-            return txtAddress.Text;
-        }
-        
+
+       
         private void btnOK_Click(object sender, EventArgs e)
         {
             string strAddress = txtAddress.Text, strLat = txtLat.Text, strLong = txtLong.Text; 
-            if(rbtnAdd.Checked)
+            if(rbtnAddr.Checked)
             {
-                this.webBView.Navigate("http://maps.google.com/maps?q=" + Uri.EscapeUriString(strAddress));
+                if(strAddress == "")
+                {
+                    MessageBox.Show("Bạn phải nhập vào địa chỉ tìm kiếm", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    //show google map based on address
+                    this.webBView.Navigate("http://maps.google.com/maps?q=" + Uri.EscapeUriString(strAddress));
+                }
+                
             }
-            else
+            if(rbtnCoordinate.Checked)
             {
-                this.webBView.Navigate("http://maps.google.com/maps?q=" + strLat + "%2C" + strLong);
+                if(strLat == "" || strLong == "")
+                {
+                    MessageBox.Show("Bạn phải nhập vào tọa độ cần tìm tìm kiếm", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    //show google map based on coordinate
+                    this.webBView.Navigate("http://maps.google.com/maps?q=" + strLat + "%2C" + strLong);
+                }
+                
             }
         }
-
+                
         private void rbtnCoordinate_CheckedChanged(object sender, EventArgs e)
         {
             grbCoordinate.Enabled = true;
             txtAddress.Enabled = false;
+            txtAddress.Text = "";
         }
 
         private void rbtnAdd_CheckedChanged(object sender, EventArgs e)
         {
             grbCoordinate.Enabled = false;
             txtAddress.Enabled = true;
+            txtLat.Text = "";
+            txtLong.Text = "";
+        }
+
+        private void getCoordinateFromAddress()
+        {
+            //get Json from url
+            var json = new WebClient().DownloadString("https://maps.googleapis.com/maps/api/geocode/json?address=" + txtAddress.Text + "&key=AIzaSyDI_TOoqGJO6gqKv0gC7qtRctw_5FpjSWE");
+            //convert jSon to class
+            RootObject r = JsonConvert.DeserializeObject<RootObject>(json);
+            var lat = r.results[0].geometry.location.lat;
+            var lng = r.results[0].geometry.location.lng;
+            txtLat.Text = lat.ToString();
+            txtLong.Text = lng.ToString();
+        }
+
+        private void getAddressFromCoordinate()
+        {
+            //get Json from url
+            var json = new WebClient().DownloadString("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + txtLat.Text + "," + txtLong.Text + "& key=AIzaSyDI_TOoqGJO6gqKv0gC7qtRctw_5FpjSWE");
+            //convert jSon to class
+            RootObject r = JsonConvert.DeserializeObject<RootObject>(json);
+            txtAddress.Text = r.results[0].formatted_address;
+        }
+
+        private void getIpAddress(ref string IpAddress)
+        {
+            //get Json from url
+            var json = new WebClient().DownloadString("https://api.ipify.org/?format=json ");
+            //convert Json to class
+            RootObject r = JsonConvert.DeserializeObject<RootObject>(json);
+            IpAddress = r.ip;
+            rbtnGetIp.Text += " " + IpAddress;
+        }
+
+        private void getCoordinateFromIp()
+        {
+            //get Json from url
+            var json = new WebClient().DownloadString("http://freegeoip.net/json/"+IpAddress);
+            //convert Json to class
+            RootObject r = JsonConvert.DeserializeObject<RootObject>(json);
+            var lat = r.latitude;
+            var lng = r.longitude;
+            txtLat.Text = Convert.ToString(lat);
+            txtLong.Text = Convert.ToString(lng);
+        }
+
+        private void bntGet_Click(object sender, EventArgs e)
+        {
+            if(rbtnGetAddress.Checked)
+            {
+                getAddressFromCoordinate();
+            }
+            if(rbtnGetCoordinate.Checked)
+            {
+                getCoordinateFromAddress();
+            }
+            if(rbtnGetIp.Checked)
+            {                
+                getCoordinateFromIp();
+            }
+        }
+
+        private void rbtnGetCoordinate_CheckedChanged(object sender, EventArgs e)
+        {
+            if(txtAddress.Text == "" && rbtnGetCoordinate.Checked)
+            {
+                DialogResult dlr = MessageBox.Show("Bạn phải nhập vào địa chỉ", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                if(dlr == DialogResult.OK)
+                {
+                    rbtnGetCoordinate.Checked = false;
+                }
+            }
+        }
+
+        private void rbtnGetAddress_CheckedChanged(object sender, EventArgs e)
+        {
+            if (txtLat.Text == "" && txtLong.Text == "" && rbtnGetAddress.Checked)
+            {
+                DialogResult dlr = MessageBox.Show("Bạn phải nhập vào tọa độ", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                if (dlr == DialogResult.OK)
+                {
+                    rbtnGetAddress.Checked = false;
+                }
+            }
+        }
+
+        private void rbtnGetIp_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
